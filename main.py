e61e5e8c764a9683dd1fed328275937344501afb
metasisyphus <eoinmccabe.finance@gmail.com>
2026-07-16 17:27:52 +0100

    data transformation functions added to main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from ml-world-cup-predictor!")
+from ml_world_cup_predictor.feature_engineering import generate_match_features
+from ml_world_cup_predictor.data_transformation import transform_match_data 
+from ml_world_cup_predictor.config import DATA_DIRECTORY
 
+def main():
+
+    print("Hello from ml-world-cup-predictor!")
+    played,not_played = transform_match_data(DATA_DIRECTORY)
+    df = generate_match_features(played)
+    print(df.tail(5))
 
 if __name__ == "__main__":
     main()

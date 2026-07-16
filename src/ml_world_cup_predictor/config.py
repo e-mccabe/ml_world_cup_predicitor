e3e9cf615584b3383b917a3d0a04629b55127f5c
metasisyphus <eoinmccabe.finance@gmail.com>
2026-07-16 19:26:57 +0100

    Create data class to store the projects data folders: raw, interim, processed

--- src/ml_world_cup_predictor/config.py
+++ src/ml_world_cup_predictor/config.py
@@ -1,32 +1,54 @@
 
 from pathlib import Path
+from dataclasses import dataclass
+import datetime as dt
 
-
-def find_root(marker = 'pyproject.toml'):
+def _find_root(marker = 'pyproject.toml'):
     path = Path(__file__).resolve()
     for parent in path.parents:
         if (parent / marker).exists():
             return parent
     raise FileNotFoundError(f'No file: {marker} found above {__file__}')
 
-PROJECT_ROOT = find_root()
+PROJECT_ROOT = _find_root()
+
+KAGGLE_LINK = "martj42/international-football-results-from-1872-to-2017"
+
+@dataclass(frozen=True)
+class Paths:
+    raw:Path        = PROJECT_ROOT / 'data' / 'raw'
+    interim:Path    = PROJECT_ROOT / 'data' / 'interim'
+    processed:Path  = PROJECT_ROOT / 'data' / 'processed'
+
+PATH = Paths()
+
 DATA_DIRECTORY = PROJECT_ROOT / 'data' / 'international-football-results'
 PROCESSED_DIRECTORY = PROJECT_ROOT / 'data' / 'processed'
 
-STARTING_ELO = 1500
 
-FORM_WINDOW = 5
-
+# Filtering Parameters
 MINIMUM_GAMES_PLAYED = 30
+
+WC_START = dt.datetime(2026,6,11)
+
+
+# ELO Parameters
+STARTING_ELO = 1500
 
 GAME_WEIGHTS = {
     'Friendly':20,
-    'FIFA World Cup':60,
-    'FIFA World Cup qualification':40,
+    'FIFA World Cup':90,
+    'FIFA World Cup qualification':55,
     'Copa América':50,
-    'African Cup of Nations':50,
+    'African Cup of Nations':40,
     'UEFA Nations League':50,
-    'CONCACAF Nations League':50,
+    'CONCACAF Nations League':40,
     'UEFA Euro':50,
-    'CONCACAF Championship':50
+    'CONCACAF Championship':40
 }
+
+
+
+# Form Parameters
+FORM_WINDOW = 15
+
